eb8f1dcef29c07584445d58cc94bbfab11913fe9
deveshjoshii <devesh.joshi@iquanti.com>
2024-12-11 17:42:52 +0530

    parameterized the congfig.js for base url

--- cypress.config.js
+++ cypress.config.js
@@ -140,7 +140,7 @@ async function insertDataIntoDatabase(sheetData) {
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://chipadvisorreffered.smallbizvoices.com/',
+    baseUrl: process.env.BASE_URL || 'https://dev1.credello.com/', // Default fallback if not provided
     setupNodeEvents(on, config) {
       on('task', {
         async readGoogleSheet() {
